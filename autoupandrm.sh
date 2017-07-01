0253d49c6bfe015ad9b09bbe500eb6a59977924e
Eren <julian07027@gmail.com>
2017-07-02 00:33:26 +0800

    Add shell

---
+++ autoupandrm.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+cd /e/GitHub/upandrm
+
+while true
+do
+	fc="$(ls pic | wc -l)" # count file in /pic
+	if [ $fc -gt 0 ];      # $fc > 0
+		then
+		FILES=/e/GitHub/upandrm/pic/*
+		for f in $FILES
+		do
+			/mingw64/bin/git add $f
+			/mingw64/bin/git commit -m "$(date +%H-%M-%S)"
+			/mingw64/bin/git remote add origin git@github.com:meliodaseren/upandrm.git
+			/mingw64/bin/git push -u origin master
+			/mingw64/bin/git remote remove origin
+		done
+	fi;
+	rm -rf /e/GitHub/upandrm/pic/*
+	sleep 3
+done
